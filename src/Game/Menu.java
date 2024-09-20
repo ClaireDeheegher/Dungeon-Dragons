676fd3dbb97926a7f1356e07294e4009649921c3
Claire Deheegher <claire.deheegher@le-campus-numerique.fr>
2024-09-20 16:26:46 +0200

    added in comment the steps to do in menu class before initialzing in main

--- src/Game/Menu.java
+++ src/Game/Menu.java
@@ -5,9 +5,30 @@ import Game.Character_settings.Character;
 import java.util.Scanner;
 
 public class Menu extends Game {
+    /*
+    Créer le bienvenue du jeu.
+    Créer l'initialisation du personnage :
+        Demander si on souhaite créer un personnage :
+            Mettre un input oui/non.
+            Si oui, lancer la méthode de création du personnage.
+            Si non, arrêter le programme.
+        Demander à mettre un nom.
+            Input nom
+        Afficher les classes disponibles.
+        Demander si on souhaite créer un guerrier.
+            Mettre un input oui/non.
+            Si oui, la variable job stockera la valeur "guerrier".
+            Si non, demander à devenir magicien.
+                Si oui stocker la valeur magicien dans job.
+                Si non arrêter le programme.
+        Afficher le résultat post choix. Demander s'il convient.
+            Si oui, passer au programme suivant.
+            Si non, relancer le programme de création.
 
-
-
+     */
+    /*
+    Lancer la partie
+     */
 }
 
 
